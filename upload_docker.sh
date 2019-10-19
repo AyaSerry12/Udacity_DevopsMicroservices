f4033639aa1f055258f41ddf449dc0401b3cebf4
Aya Serry <aya.serry@valeo.com>
2019-10-19 02:30:52 +0200

    Task#4 Push Image

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,16 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=ml_project
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+sudo $(aws ecr get-login --no-include-email --region us-west-2)
+sudo docker tag $dockerpath:latest 570465604558.dkr.ecr.us-west-2.amazonaws.com/$dockerpath:latest
+
+
 
 # Step 3:
 # Push image to a docker repository
+sudo docker push 570465604558.dkr.ecr.us-west-2.amazonaws.com/$dockerpath:latest
