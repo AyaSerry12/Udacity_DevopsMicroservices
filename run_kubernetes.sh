f3ac53dd0287468b453832111e9ffcba5b10c59e
Aya Serry <aya.serry@valeo.com>
2019-10-19 04:31:05 +0200

    Task#6 run_kubernetes file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=ml_project
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+sudo kubectl run app --image=ayaserry/$dockerpath:latest --port=80
 
 # Step 3:
 # List kubernetes pods
+sudo kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+sudo kubectl expose deployment app --type=LoadBalancer --port=8000 --target-port=80
