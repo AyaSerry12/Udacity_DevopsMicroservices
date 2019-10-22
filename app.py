7a2ff9ff99be71391b6c96a7565b385e56ec2c63
Aya Serry <aya.serry@valeo.com>
2019-10-23 00:05:56 +0200

    App print

--- app.py
+++ app.py
@@ -63,9 +63,8 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
-    jsonPre=jsonify({'prediction': prediction})
-    print("Json payload:" + jsonPre)
-    return jsonPre
+	LOG.info(f"prediction {prediction}")
+    return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
     # load pretrained model as clf
